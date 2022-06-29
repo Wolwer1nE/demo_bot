152873faafcf8b54a085b42481b0cbaed8ce442e
Wolwer1ne <wolwerine@yandex.ru>
2022-06-29 16:11:26 +0300

    Patch updates poller

--- client_patch.rb
+++ client_patch.rb
@@ -9,5 +9,16 @@ module Telegram
         @base_uri = format(URL_TEMPLATE, server: server, token: self.token)
       end
     end
+    class UpdatesPoller
+      def fetch_updates(offset = self.offset)
+        response = bot.async(false) { bot.get_updates(offset: offset, timeout: timeout) }
+        response.is_a?(Array) ? response : response['result']
+      rescue RuntimeError => e
+        log { 'Network problem' }
+        warn e.message
+        nil
+      end
+    end
   end
+
 end
